0450791c3c5c50d0017fc534dc539c50840da342
Sandro Vieira de Paula <sandrovp@gmail.com>
2025-05-26 15:46:07 -0300

    fix: remover altura da tela do contêiner de login para melhor responsividade

--- app/sign-in/[[...sign-in]]/page.tsx
+++ app/sign-in/[[...sign-in]]/page.tsx
@@ -2,7 +2,7 @@ import { SignIn } from "@clerk/nextjs";
 
 export default function Page() {
   return (
-    <div className="flex justify-center items-center w-screen h-screen">
+    <div className="flex justify-center items-center w-screen">
       <SignIn />
     </div>
   );
